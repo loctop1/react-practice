200cdf27e6b721a5563e7be19be6094111b9baa2
loctop1 <loc2003k22@gmail.com>
2023-10-07 16:01:13 +0700

    Part 4: Paginate Users

--- src/Components/TableUsers.js
+++ src/Components/TableUsers.js
@@ -1,25 +1,45 @@
 import Table from 'react-bootstrap/Table';
 import { useEffect, useState } from 'react';
 import { fetchAllUser } from '../service/UserService';
+//Icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleChevronRight, faCircleChevronLeft } from '@fortawesome/free-solid-svg-icons';
+//Phân trang
+import ReactPaginate from 'react-paginate';
 
 const TableUsers = (props) => {
     const [listUsers, setListUsers] = useState([]);
     /**Khai báo một state "listUsers" và hàm "setListUsers" để lưu trữ danh sách người dùng. */
+    const [totalUsers, setTotalUsers] = useState(0);
+    /**Khai báo một state "totalUsers" và hàm "setTotalUsers" để lưu trữ tổng số người dùng*/
+    const [totalPages, setTotalPages] = useState(0);
+    /**Khai báo một state "totalUsers" và hàm "setTotalUsers" để lưu trữ tổng số trang*/
     useEffect(() => {
-        getUsers();
+        getUsers(1);
     }, []);
     /**Sử dụng useEffect để gọi hàm "getUsers" khi component được tạo ra lần đầu. */
 
-    const getUsers = async () => {
-    /**Định nghĩa hàm "getUsers" là một hàm bất đồng bộ sử dụng async/await. */ 
-        let res = await fetchAllUser();
-        /**Gọi hàm "fetchAllUser" (truyền từ component khác) để tải danh sách người dùng từ máy chủ. */
+    const getUsers = async (page) => {
+        /**Định nghĩa hàm "getUsers" là một hàm bất đồng bộ sử dụng async/await. */
+        let res = await fetchAllUser(page);
+        /**Gọi hàm "fetchAllUser" (truyền từ component khác) để tải danh sách người dùng từ máy chủ.
+         * page này là được truyền ở component UserService*/
         if (res && res.data) {
             setListUsers(res.data)
             /**res.data chứa danh sách người dùng từ phản hồi. */
+            setTotalUsers(res.total) //tổng số User
+            setTotalPages(res.total_pages) //tổng số trang
         }
         /**Kiểm tra xem phản hồi có dữ liệu và gán dữ liệu vào state "listUsers" nếu có. */
         console.log('>>> check res: ', res);
+    }
+
+    const handlePageClick = (event) => {
+        console.log('>>> check event: ', event);
+        getUsers(+event.selected + 1);
+        /**+event.selected sẽ chuyển đổi giá trị event.selected thành một số.
+         * +event.selected + 1 sẽ thêm 1 đơn vị vào giá trị event.selected, có thể là để tăng trang 
+         * lên 1 nếu event.selected biểu thị trang trước đó.*/
     }
     return (
         <>
@@ -40,7 +60,7 @@ const TableUsers = (props) => {
                                 <tr key={`users-${index}`}>
                                     <td>{item.id}</td>
                                     <td>
-                                        <img src={item.avatar} className='rounded-circle'/>
+                                        <img src={item.avatar} className='rounded-circle' />
                                     </td>
                                     <td>{item.email}</td>
                                     <td>{item.first_name}</td>
@@ -51,6 +71,30 @@ const TableUsers = (props) => {
                     }
                 </tbody>
             </Table>
+            <ReactPaginate
+                nextLabel={
+                    <FontAwesomeIcon icon={faCircleChevronRight} size='xl' style={{ color:  '#005eff' }} />
+                }
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                pageCount={totalPages}
+                previousLabel={
+                    <FontAwesomeIcon icon={faCircleChevronLeft} size='xl' style={{ color:  '#005eff' }} />
+                }
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination justify-content-center"
+                activeClassName="active"
+                renderOnZeroPageCount={null}
+            />
         </>
     )
 }
--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,8 +1,10 @@
 import axios from "./customize-axios";
 
-const fetchAllUser = () => {
+const fetchAllUser = (page) => {
     return(
-        axios.get('/api/users?page=1')
+        axios.get(`/api/users?page=${page}`)
+        /**page=${page} là một tham số truy vấn được thêm vào URL để xác định trang dữ liệu bạn 
+         * muốn lấy. */
     )
 }
 export {fetchAllUser};
